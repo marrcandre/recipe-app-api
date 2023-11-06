e6c8db03fdcb1d0ab2eedce91d8b86080992288c
Marco André Mendes <marcoandre@gmail.com>
2023-11-06 15:02:20 -0300

    Refactor code for better readability and PEP8 compliance.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,7 +12,9 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful."""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -39,7 +41,9 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "test123"
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
