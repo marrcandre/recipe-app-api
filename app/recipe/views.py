9c1d56fb327b336f8029280d1b158f0204e1a507
Marco André Mendes <marcoandre@gmail.com>
2024-05-27 09:48:34 -0300

    chore: Update ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -55,7 +55,8 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin,
+class IngredientViewSet(mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
                         viewsets.GenericViewSet):
     """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
