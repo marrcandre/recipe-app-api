34741064383b472775bfb9a8f900b1cb18239d0d
Marco André Mendes <marcoandre@gmail.com>
2024-06-04 14:31:43 -0300

    feat: Update Dockerfile and settings for static file handling

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -23,3 +26,9 @@ urlpatterns = [
     path("api/user/", include("user.urls")),
     path("api/recipe/", include("recipe.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
